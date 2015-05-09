40ce9e99c3a3eb9237f0ea1134475f88b6b1da54
Simon Weiler <mail@simon-weiler.de>
2015-05-09 17:29:11 +0200

    Initialized Project with (nearly) empty unity project

---
+++ Assets/MoveCube.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			pos.x -= 10f * Time.deltaTime;
+		}
+
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			pos.x += 10f * Time.deltaTime;
+		}
+
+		if(Input.GetKey(KeyCode.UpArrow)) {
+			pos.y += 10f * Time.deltaTime;
+		}
+
+		if(Input.GetKey(KeyCode.DownArrow)) {
+			pos.y -= 10f * Time.deltaTime;
+		}
+
+		transform.position = pos;
+	}
+}
