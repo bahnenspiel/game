7ce3cb8f4e76f82dd67272713a6ba5b3a81390f0
Simon Weiler <mail@simon-weiler.de>
2015-05-11 12:37:59 +0200

    Moving left/right, speed and jump implemented

--- Assets/MoveCube.cs
+++ Assets/MoveCube.cs
@@ -3,30 +3,44 @@ using System.Collections;
 
 public class MoveCube : MonoBehaviour {
 
+	public float steeringSpeed = 8f;
+	public float jumpSpeed = 1000f;
+	public float drivingSpeed = 10f;
+
+	private float speed;
+	private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-	
+		speed = 0;
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+		Vector3 vel = rb.velocity;
+		vel.z = speed;
+
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			vel.x = -steeringSpeed;
+		} else if(Input.GetKey(KeyCode.RightArrow)) {
+			vel.x = steeringSpeed;
+		}
+
+		rb.velocity = vel;
+
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			rb.AddForce(new Vector3(0,jumpSpeed,0));
+		}
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 pos = transform.position;
-		if(Input.GetKey(KeyCode.LeftArrow)) {
-			pos.x -= 10f * Time.deltaTime;
-		}
-
-		if(Input.GetKey(KeyCode.RightArrow)) {
-			pos.x += 10f * Time.deltaTime;
-		}
-
 		if(Input.GetKey(KeyCode.UpArrow)) {
-			pos.y += 10f * Time.deltaTime;
+			speed += drivingSpeed * Time.deltaTime;
+		} else if(Input.GetKey(KeyCode.DownArrow)) {
+			speed -= drivingSpeed * Time.deltaTime;
 		}
 
-		if(Input.GetKey(KeyCode.DownArrow)) {
-			pos.y -= 10f * Time.deltaTime;
-		}
-
-		transform.position = pos;
 	}
 }
